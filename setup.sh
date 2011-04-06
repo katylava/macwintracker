a6cfc8085712236eea485ba4fe6cd36f61d3b02b
katy lavallee <katylava@gmail.com>
2011-04-06 12:44:33 -0500

    add pythonpath to launchd plist. fix directory paths in setup.sh.

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@
 
 user=`whoami`
 bid='com.katylavallee.wintracker'
-dest="~/Library/Application Support/$bid"
+dest=~/Library/Application\ Support/$bid
+pypath=`python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`
 [ -z $1 ] && intvl='3' || intvl=$1
 
 
-cd "${0##*/}"
+cd "$(dirname $0)"
 [ ! -d "${dest}" ] && mkdir "${dest}"
 cp -f wintracker.py "${dest}/"
 cp -f $bid.plist ~/Library/LaunchAgents
@@ -14,5 +15,6 @@ cp -f $bid.plist ~/Library/LaunchAgents
 cd ~/Library/LaunchAgents
 sed -i "s/{{USER}}/$user/" $bid.plist
 sed -i "s/{{INTERVAL}}/$intvl/" $bid.plist
+sed -i "s:{{PYTHONPATH}}:$pypath:" $bid.plist
 
 launchctl load $bid.plist
