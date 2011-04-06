2c97dc40723c5c7ee127559b6bf711f593097b8d
katy lavallee <katylava@gmail.com>
2011-04-06 16:55:09 -0500

    added uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+bid='com.katylavallee.wintracker'
+dest=~/Library/Application\ Support/$bid
+
+running=`launchctl list | grep $bid`
+[ ! -z "${running}" ] && launchctl unload ~/Library/LaunchAgents/$bid.plist
+
+rm -f ~/Library/LaunchAgents/$bid.plist
+rm -fr "${dest}/$bid"
