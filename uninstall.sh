a6dcf817e6d46922de7ef69b22e632d29aa17944
katy lavallee <katylava@gmail.com>
2011-04-06 17:01:07 -0500

    fixed path in uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -7,4 +7,4 @@ running=`launchctl list | grep $bid`
 [ ! -z "${running}" ] && launchctl unload ~/Library/LaunchAgents/$bid.plist
 
 rm -f ~/Library/LaunchAgents/$bid.plist
-rm -fr "${dest}/$bid"
+rm -fr "${dest}"
