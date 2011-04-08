00cf3142387501fd59d0f71e739d1a7f61761bd0
katy lavallee <katylava@gmail.com>
2011-04-08 12:48:49 -0500

    fixed 'throttling respawn' errors by settings launchd interval to > 10 seconds and telling wintracker.py to run [launchd interval]/[wintracker interval] times before exiting

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+launchdIntvl=30
 user=`whoami`
 bid='com.katylavallee.wintracker'
 dest=~/Library/Application\ Support/$bid
 pypath=`python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`
 pythex=`which python`
 [ -z $1 ] && intvl='3' || intvl=$1
-
+limit=$(($launchdIntvl/$intvl))
 
 cd "$(dirname $0)"
 [ ! -d "${dest}" ] && mkdir "${dest}"
@@ -15,6 +16,8 @@ cp -f $bid.plist ~/Library/LaunchAgents
 
 cd ~/Library/LaunchAgents
 sed -i "s/{{USER}}/$user/" $bid.plist
+sed -i "s/{{LIMIT}}/$limit/" $bid.plist
+sed -i "s/{{LAUNCHDINTVL}}/$launchdIntvl/" $bid.plist
 sed -i "s/{{INTERVAL}}/$intvl/" $bid.plist
 sed -i "s:{{PYTHONPATH}}:$pypath:" $bid.plist
 sed -i "s:{{PATH}}:${PATH//\:/\\:}:" $bid.plist
