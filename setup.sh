353c4ebd86116d39f76dc1584f2d22b4c85e5aac
katy lavallee <katylava@gmail.com>
2011-04-28 18:02:26 -0500

    organized files into directories

--- setup.sh
+++ setup.sh
@@ -11,20 +11,16 @@ limit=$(($launchdIntvl/$intvl))
 
 cd "$(dirname $0)"
 [ ! -d "${dest}" ] && mkdir "${dest}"
-cp -f wintracker.py "${dest}/"
-cp -f jsonlogdedupe.py "${dest}/"
-cp -f uninstall.sh "${dest}/"
-cp -f $bid.plist ~/Library/LaunchAgents
-cp -f ${bid}LogRotate.plist ~/Library/LaunchAgents
+cp -f bin/* "${dest}/"
+cp -f launchagents/* ~/Library/LaunchAgents
 
 cd ~/Library/LaunchAgents
-sed -i "s/{{USER}}/$user/" $bid.plist
-sed -i "s/{{USER}}/$user/" ${bid}LogRotate.plist
+sed -i "s/{{USER}}/$user/" $bid*.plist
+sed -i "s:{{PATH}}:${PATH//\:/\\:}:" $bid*.plist
+sed -i "s:{{PYTHONPATH}}:$pypath:" $bid*.plist
 sed -i "s/{{LIMIT}}/$limit/" $bid.plist
 sed -i "s/{{LAUNCHDINTVL}}/$launchdIntvl/" $bid.plist
 sed -i "s/{{INTERVAL}}/$intvl/" $bid.plist
-sed -i "s:{{PYTHONPATH}}:$pypath:" $bid.plist
-sed -i "s:{{PATH}}:${PATH//\:/\\:}:" $bid.plist
 sed -i "
 1 {
 c\
