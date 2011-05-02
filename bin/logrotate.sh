fc1c80a962e0af9819fff6eceb6412adbcaf9f1d
katy lavallee <katylava@gmail.com>
2011-05-02 14:46:09 -0500

    needed quotes in logrotate.sh, since directory name has spaces

--- bin/logrotate.sh
+++ bin/logrotate.sh
@@ -5,6 +5,6 @@ cmddir=~/Library/Application\ Support/com.katylavallee.wintracker
 filedate=`date +%Y%m%d-%H%M%S`
 
 cd "${logdir}"
-${cmddir}/jsonlogdedupe.py -i ts wintracker.log > wintracker.$filedate.log && :> wintracker.log
+"${cmddir}/jsonlogdedupe.py" -i ts wintracker.log > wintracker.$filedate.log && :> wintracker.log
 cat wintracker_err.log > wintracker_err.$filedate.log && :> wintracker_err.log
 
