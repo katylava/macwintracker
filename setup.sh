f432e4992ae8712f3769bd7023f1f8962e06368e
katy lavallee <katylava@gmail.com>
2011-05-19 15:01:49 -0500

    removed refs in setup to unused params. run uninstall before setup.

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-launchdIntvl=30
 user=`whoami`
 bid='com.katylavallee.wintracker'
 dest=~/Library/Application\ Support/$bid
 pypath=`python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()"`
 pythex=`which python`
 [ -z $1 ] && intvl='3' || intvl=$1
-limit=$(($launchdIntvl/$intvl))
 
 cd "$(dirname $0)"
 [ ! -d "${dest}" ] && mkdir "${dest}"
@@ -18,8 +16,6 @@ cd ~/Library/LaunchAgents
 sed -i "s/{{USER}}/$user/" $bid*.plist
 sed -i "s:{{PATH}}:${PATH//\:/\\:}:" $bid*.plist
 sed -i "s:{{PYTHONPATH}}:$pypath:" $bid*.plist
-sed -i "s/{{LIMIT}}/$limit/" $bid.plist
-sed -i "s/{{LAUNCHDINTVL}}/$launchdIntvl/" $bid.plist
 sed -i "s/{{INTERVAL}}/$intvl/" $bid.plist
 sed -i "
 1 {
@@ -32,5 +28,9 @@ c\
 #!$pythex
 }" "${dest}/jsonlogdedupe.py"
 
+
+
+cd "$(dirname $0)"
+./uninstall.sh
 launchctl load $bid.plist
 launchctl load ${bid}LogRotate.plist
