639b69cabbbb6462e9dc52e128bfbbeb47b0ebea
katy lavallee <katylava@gmail.com>
2011-05-19 16:05:15 -0500

    json log deduper now includes option to choose which line (first or last) of a series of duplicate lines should be output

--- bin/jsonlogdedupe.py
+++ bin/jsonlogdedupe.py
@@ -15,23 +15,35 @@ if __name__ == '__main__':
     from optparse import OptionParser
     parser = OptionParser(usage="usage: %prog [options...] <file>")
     parser.add_option('-i', '--ignore-key', action='append', type='string', help='ignore this key when comparing json lines')
+    parser.add_option('-f', '--print-first', action='store_true', dest='match_first', default=True, help='output only the first line of a series of duplicates')
+    parser.add_option('-l', '--print-last', action='store_false', dest='match_first', help='output only the last line of a series of duplicates')
     (options, args) = parser.parse_args()
 
     logfile = args[0]
     ignore = options.ignore_key
 
     f = file(logfile, 'r')
-    l = f.next()
-    print l,
+    xline = f.next()
+    if options.match_first:
+        print xline,
 
-    x = json.loads(l)
-    y = None
-    for l in f:
-        y = json.loads(l)
-        dupe = compare(x, y, ignore)
-        if not dupe:
-            print l,
-        x = y
+    xjson = json.loads(xline)
+    yjson = None
+    for yline in f:
+        try:
+            yjson = json.loads(yline)
+        except ValueError:
+            # skip lines with json errors
+            pass
+        else:
+            dupe = compare(xjson, yjson, ignore)
+            if not dupe:
+                if options.match_first:
+                    print yline,
+                else:
+                    print xline,
+            xjson = yjson
+            xline = yline
 
 
 
